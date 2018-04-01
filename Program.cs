d894e50c1b2eee71621609da4db7a314f907e830
tsolsh <adi.tsol1@gmail.com>
2018-04-01 23:02:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            ServiceBase[] ServicesToRun = new ServiceBase[] { new ImageService(args) };
+            /*ServicesToRun = new ServiceBase[]
+            {
+                new MyNewService()
+            };*/
+            ServiceBase.Run(ServicesToRun);
+          
+        }
+    }
+}
